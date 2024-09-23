39fd8177388ab0bb92a24c84fcd5b1e214d5548b
sasikumar9951 <sasikumar.s9951@gmail.com>
2024-09-23 22:09:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var overlay=document.querySelector(".overlay")
+var addbook=document.querySelector(".addbook")
+var btn=document.getElementById("btn")
+var cancel =document.getElementById("cancel")
+var container=document.querySelector(".container")
+var containerbox=document.querySelector(".containerbox")
+var add=document.getElementById("add")
+var bookname=document.getElementById("bookname")
+var author=document.getElementById("author")
+var describe=document.getElementById("describe")
+var form=document.querySelector("form")
+
+
+btn.addEventListener("click",function () {
+  overlay.style.display = "block";
+  addbook.style.display = "block";
+
+});
+
+cancel.addEventListener("click",function(event){
+    event.preventDefault()
+    overlay.style.display="none"
+    addbook.style.display="none"
+     form.reset();
+})
+
+add.addEventListener("click",function(event){
+    event.preventDefault()
+    var div=document.createElement("div")
+    div.setAttribute("class","containerbox")
+    div.innerHTML=`<h2>${bookname.value}</h2><h3>${author.value}</h3><p>${describe.value}</p><button onclick="saya(event)">Delete</button>`
+    container.append(div)
+    overlay.style.display="none"
+    addbook.style.display="none"
+    form.reset()
+    
+
+})
+function saya(event){
+    event.target.parentElement.remove()
+}
+
+
+
+
+
